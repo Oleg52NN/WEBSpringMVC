b6fa2e64d5059531ef4fb41fded5fd5a56826ba3
Oleg NN <zvoprosno@gmail.com>
2023-04-16 00:44:17 +0300

    notFound

--- src/main/java/dz/servlets/exception/NotFoundException.java
+++ src/main/java/dz/servlets/exception/NotFoundException.java
@@ -1,6 +1,10 @@
 package dz.servlets.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
+
     public NotFoundException() {
     }
 
